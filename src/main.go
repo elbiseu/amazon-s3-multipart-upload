5d3f6de40156768c451741fab616a54b3ffd849d
Elvis Sánchez <elvis.sanchez.napoles@gmail.com>
2021-09-27 14:47:50 -0500

    Update Message struct

--- src/main.go
+++ src/main.go
@@ -32,32 +32,27 @@ type Link struct {
 }
 
 type Message struct {
-	Id     string `json:"id"`
+	Key    string `json:"id"`
 	Status string `json:"status"`
 	Links  []Link `json:"links"`
 }
 
 func FileHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-
 	switch r.Method {
 	case http.MethodPost:
 		contentType := r.Header.Get("Content-Type")
-
 		if !strings.HasPrefix(contentType, "image/") && !strings.HasPrefix(contentType, "video/") {
 			w.WriteHeader(http.StatusUnsupportedMediaType)
 			return
 		}
-
 		if r.ContentLength > maxContentSize {
 			w.WriteHeader(http.StatusRequestEntityTooLarge)
 			return
 		}
-
-		id := uuid.New().String()
 		multipartUploadOutput, err := client.CreateMultipartUpload(ctx, &s3.CreateMultipartUploadInput{
 			Bucket:                    aws.String(bucket),
-			Key:                       aws.String(id),
+			Key:                       aws.String(uuid.New().String()),
 			ACL:                       types.ObjectCannedACLPrivate,
 			BucketKeyEnabled:          false,
 			CacheControl:              nil,
@@ -91,14 +86,13 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		var buffer bytes.Buffer
 		var completedParts []types.CompletedPart
 		var lastPart bool
 		var partNumber int32 = 1 // The first part number must always start with 1.
-
 		for !lastPart {
 			n, err := io.CopyN(&buffer, r.Body, 4096)
+			// The io.EOF error occurs when the stream has reached its end.
 			if n == 0 || err == io.EOF {
 				lastPart = true
 			} else if err != nil {
@@ -106,7 +100,8 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-
+			// If the buffer has the minimum required size or the current part is the last one,
+			// a new part is stored in the bucket.
 			if buffer.Len() > minUploadPartSize || lastPart {
 				uploadPartOutput, err := client.UploadPart(ctx, &s3.UploadPartInput{
 					Bucket:               multipartUploadOutput.Bucket,
@@ -127,12 +122,10 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 					w.WriteHeader(http.StatusInternalServerError)
 					return
 				}
-
 				completedParts = append(completedParts, types.CompletedPart{
 					ETag:       uploadPartOutput.ETag,
 					PartNumber: partNumber,
 				})
-
 				// The buffer is empty to the next parts.
 				buffer.Reset()
 				partNumber++
@@ -154,9 +147,8 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		msg := Message{
-			Id:     id,
+			Key:    *completeMultipartUploadOutput.Key,
 			Status: "Created",
 			Links: []Link{
 				{
@@ -165,22 +157,18 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 				},
 			},
 		}
-
 		b, err := json.Marshal(msg)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		w.WriteHeader(http.StatusCreated)
-
 		if _, err := w.Write(b); err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		return
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
